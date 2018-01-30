e1ec89c71848aa62dbd56ad3d704ab5cff0844e9
Sean Cesmat <sean.cesmat@gmail.com>
2018-01-30 10:05:24 -0800

    You can now save the current state

--- js/app.js
+++ js/app.js
@@ -1,74 +1,83 @@
 $(document).ready(function() {
-  var itemList = {};
+  //var itemList = {};
   var currentState = {};
-  var itemID = 0;
+  var isChecked = {};
+  var itemID = Math.floor(Math.random() * 1000);
+  var uncheckedIcon = '<i class="fas fa-square"></i>';
+  var checkedIcon = '<i class="fas fa-check-square"></i>';
+  var deleteIcon = '<i class="delete far fa-times-circle"></i>';
 
   var addItem = function(e) {
     e.preventDefault();
-    var newListItem = $('<li class="list-item" ' + 'data-item-id="' + itemID +'">');
-    $('ul').append(newListItem.text($('#new-todo').val()).prepend('<i class="fas fa-square"></i>').append('<i class="delete far fa-times-circle"></i>'));
-    itemList[itemID] = $('#new-todo').val();
-    console.log(itemList);
+    var newListItem = $('<li class="list-item" ' + 'id="' + itemID + '"' + 'data-item-id="' + itemID +'">');
+    $('ul').append(newListItem.text($('#new-todo').val()).prepend(uncheckedIcon).append(deleteIcon));
+    //itemList[itemID] = $('#new-todo').val();
     $('#new-todo').val('').focus();
     $(newListItem).click(checkBox);
     $('.delete').click(removeItem);
     itemID++;
-    console.log(itemID);
   }
-
-  var checkBox = function() {
-    //console.log(this);
-    $(this).children('.fas').toggleClass('fa-square');
-    $(this).children('.fas').toggleClass('fa-check-square');
-    $(this).toggleClass('checked');
-  }
-
-  var removeItem = function(li) {
-    //console.log($('.delete').parent());
-    $(this).parent().remove();
-    //console.log($(this).parent().attr('data-item-id'));
-    delete itemList[$(this).parent().attr('data-item-id')];
-    console.log(itemList);
-  }
-
+  // Add click event to submit button
   $('#add-item-btn').click(addItem);
+  // Make it so you can hit the enter key
   $('#new-todo').keyup(function(e){
       if (e.keyCode === 13) {
           addItem(e);
       }
   });
+
+  // Toggle the icon checked/unchecked and add or remove class
+  var checkBox = function() {
+    $(this).children('.fas').toggleClass('fa-square');
+    $(this).children('.fas').toggleClass('fa-check-square');
+    $(this).toggleClass('checked');
+  }
+
+  // Called from the delete button click event
+  var removeItem = function(li) {
+    $(this).parent().remove();
+    //delete itemList[$(this).parent().attr('data-item-id')];
+  }
+
+  // Delete Button Click Event
   $('ul').on('click', '.delete', function(e) {
     var item = this;
     removeItem(e, item);
   });
 
-  $( "ul" ).sortable();
-  $( "ul" ).disableSelection();
+  $('#save-list').click(function() {
+    // Put the object into storage
+    $('.list-item').each(function() {
+      var string = $(this).text();
+      currentState[$(this).attr('data-item-id')] = string;
+      if ($(this).hasClass('checked')) {
+        isChecked[$(this).attr('data-item-id')] = true;
+      } else {
+        isChecked[$(this).attr('data-item-id')] = false;
+      }
+    });
+    localStorage.setItem('currentState', JSON.stringify(currentState));
+    localStorage.setItem('isChecked', JSON.stringify(isChecked));
+  });
 
-  // $('#save-list').click(function() {
-  //   // Put the object into storage
-  //   //localStorage.setItem('itemList', JSON.stringify(itemList));
-  //
-  //   $('.list-item').each(function() {
-  //     console.log($(this).val());
-  //     var string = $(this).text();
-  //     //currentState[$(this).attr('data-item-id')] = string;
-  //     if ($(this).hasClass('checked')) {
-  //       currentState[$(this).attr('data-item-id') + '-isChecked'] = true;
-  //     } else {
-  //       currentState[$(this).attr('data-item-id') + '-isChecked'] = false;
-  //     }
-  //   });
-  //   localStorage.setItem('currentState', JSON.stringify(currentState))
-  //   // Retrieve the object from storage
-  //   retrievedItemList = localStorage.getItem('currentState');
-  //
-  //   console.log('retrievedObject: ', JSON.parse(retrievedItemList));
-  // });
-  // var loadList = function() {
-  //   loadCurrentState = JSON.parse(localStorage.getItem(retrievedItemList));
-  //   console.log(loadCurrentState);
-  // }
-  //
-  // loadList();
+  var loadList = function() {
+    var retrievedItemList = JSON.parse(localStorage.getItem('currentState'));
+    var retrievedIsChecked = JSON.parse(localStorage.getItem('isChecked'));
+
+    $.each(retrievedItemList, function(i, val) {
+      if (retrievedIsChecked[i] === true) {
+        var newListItem = $('<li class="list-item checked"' + 'data-item-id="' + i + '">');
+        $('ul').append(newListItem.text(val).prepend(checkedIcon).append(deleteIcon));
+      } else if (retrievedIsChecked[i] === false) {
+        var newListItem = $('<li class="list-item" ' + 'data-item-id="' + i +'">');
+        $('ul').append(newListItem.text(val).prepend(uncheckedIcon).append(deleteIcon));
+      }
+      $(newListItem).click(checkBox);
+      $('.delete').click(removeItem);
+      i++;
+    });
+
+    $('#new-todo').val('').focus();
+  }
+  loadList();
 });
