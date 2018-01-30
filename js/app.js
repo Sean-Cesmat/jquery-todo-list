2cef2111aa455029df568dfc332721ec1b8f8d50
Sean Cesmat <sean.cesmat@gmail.com>
2018-01-30 13:41:13 -0800

    Will now save current state - including list order and checkmarks

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
-  //var itemList = {};
   var currentState = {};
   var isChecked = {};
-  var itemID = Math.floor(Math.random() * 1000);
+  var itemID = Math.floor(Math.random() * 10000);
   var uncheckedIcon = '<i class="fas fa-square"></i>';
   var checkedIcon = '<i class="fas fa-check-square"></i>';
   var deleteIcon = '<i class="delete far fa-times-circle"></i>';
 
+  // Triggered by submit button / enter key - Make a new list item and add it to the dom
   var addItem = function(e) {
     e.preventDefault();
-    var newListItem = $('<li class="list-item" ' + 'id="' + itemID + '"' + 'data-item-id="' + itemID +'">');
+    var newListItem = $('<li class="list-item" ' + 'id="' + itemID + '"' + 'data-item-id="id-' + itemID +'">');
     $('ul').append(newListItem.text($('#new-todo').val()).prepend(uncheckedIcon).append(deleteIcon));
-    //itemList[itemID] = $('#new-todo').val();
     $('#new-todo').val('').focus();
     $(newListItem).click(checkBox);
     $('.delete').click(removeItem);
@@ -34,37 +33,39 @@ $(document).ready(function() {
   }
 
   // Called from the delete button click event
-  var removeItem = function(li) {
+  var removeItem = function() {
     $(this).parent().remove();
-    //delete itemList[$(this).parent().attr('data-item-id')];
   }
 
-  // Delete Button Click Event
-  $('ul').on('click', '.delete', function(e) {
-    var item = this;
-    removeItem(e, item);
-  });
 
+  // When the save button is clicked, run this event to save current state of list
   $('#save-list').click(function() {
     // Put the object into storage
     $('.list-item').each(function() {
-      var string = $(this).text();
-      currentState[$(this).attr('data-item-id')] = string;
+      // store each list item id as key and <li> val as value
+      currentState[$(this).attr('data-item-id')] = $(this).text();
+      // if list item is checked off, store it in
       if ($(this).hasClass('checked')) {
         isChecked[$(this).attr('data-item-id')] = true;
       } else {
         isChecked[$(this).attr('data-item-id')] = false;
       }
     });
+    // Store the two objects in localStorage
     localStorage.setItem('currentState', JSON.stringify(currentState));
     localStorage.setItem('isChecked', JSON.stringify(isChecked));
   });
 
+  // When the page loads, run this function to load list items that were saved in localStorage
   var loadList = function() {
+    // Retrieve the two localStoarge objects and store them in a variable
     var retrievedItemList = JSON.parse(localStorage.getItem('currentState'));
     var retrievedIsChecked = JSON.parse(localStorage.getItem('isChecked'));
 
+    // Run through the list items and add them to the page
     $.each(retrievedItemList, function(i, val) {
+      var temp = retrievedIsChecked[i];
+      // if the item was checked, recheck that list item
       if (retrievedIsChecked[i] === true) {
         var newListItem = $('<li class="list-item checked"' + 'data-item-id="' + i + '">');
         $('ul').append(newListItem.text(val).prepend(checkedIcon).append(deleteIcon));
@@ -72,14 +73,16 @@ $(document).ready(function() {
         var newListItem = $('<li class="list-item" ' + 'data-item-id="' + i +'">');
         $('ul').append(newListItem.text(val).prepend(uncheckedIcon).append(deleteIcon));
       }
+      // Add click events to list checkbox and delete button
       $(newListItem).click(checkBox);
       $('.delete').click(removeItem);
-      i++;
     });
-
+    // after adding all the list items back, refocus on the input text field
     $('#new-todo').val('').focus();
   }
+  // using jQuery UI make the list items sortable
   $( "ul" ).sortable();
   $( "ul" ).disableSelection();
+
   loadList();
 });
